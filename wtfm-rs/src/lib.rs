f3f80f0d8d1f00d80acbc050a35f467c263ce53b
Sam Liu <ontouchstart@gmail.com>
2026-01-17 17:52:15 -0500

    update wtfm-rs/src/lib.rs

--- wtfm-rs/src/lib.rs
+++ wtfm-rs/src/lib.rs
@@ -3,33 +3,33 @@
 /// `use wtfm_rs::add;`
 ///
 /// ```sh
-/// cargo new --lib --name wtfm-test /tmp/wtfm-test
-/// cd /tmp/wtfm-test && cargo add --git https://github.com/wtfm-rs/wtfm-rs.github.io wtfm-rs && cd -
-/// mkdir /tmp/wtfm-test/tests
-/// cat > /tmp/wtfm-test/tests/it-works <<EOF
+/// cargo new --lib --name tet-wtfm-rs /tmp/test-wtfm-rs
+/// cd /tmp/test-wtfm-rs && cargo add --git https://github.com/wtfm-rs/wtfm-rs.github.io wtfm-rs && cd -
+/// mkdir /tmp/test-wtfm-rs/tests
+/// cat > /tmp/test-wtfm-rs/tests/it-works <<EOF
 /// #[test]
 /// fn it_works() {
-///     use wtfm_rs::add;
-///     assert_eq!(add(2, 2), 4);
+///    use wtfm_rs::add;
+///    assert_eq!(add(2, 2), 4);
 /// }
 /// EOF
-/// cd /tmp/wtfm-test && cargo test --tests && cd -
-/// rm -rf /tmp/wtfm-test
+/// cd /tmp/test-wtfm-rs && cargo test --tests && cd -
+/// rm -rf /tmp/test-wtfm-rs
 /// ```
 ///
 /// Local test on my mac (`~/github/wtfm-rs/wtfm-rs.github.io`)
 /// ```sh
-///    Creating library `wtfm-test` package
+///     Creating library `tet-wtfm-rs` package
 /// note: see more `Cargo.toml` keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
 ///    Updating git repository `https://github.com/wtfm-rs/wtfm-rs.github.io`
 ///      Adding wtfm-rs (git) to dependencies
 ///    Updating git repository `https://github.com/wtfm-rs/wtfm-rs.github.io`
 ///     Locking 1 package to latest Rust 1.92.0 compatible version
 /// ~/github/wtfm-rs/wtfm-rs.github.io
-///   Compiling wtfm-rs v0.1.0 (https://github.com/wtfm-rs/wtfm-rs.github.io#f02d438f)
-///   Compiling wtfm-test v0.1.0 (/private/tmp/wtfm-test)
+///   Compiling wtfm-rs v0.1.0 (https://github.com/wtfm-rs/wtfm-rs.github.io#362925b5)
+///   Compiling tet-wtfm-rs v0.1.0 (/private/tmp/test-wtfm-rs)
 ///    Finished `test` profile [unoptimized + debuginfo] target(s) in 0.24s
-///     Running unittests src/lib.rs (target/debug/deps/wtfm_test-c3f9b0c7c40b2943)
+///     Running unittests src/lib.rs (target/debug/deps/tet_wtfm_rs-2fec6c5d019a0c2d)
 ///
 /// running 1 test
 /// test tests::it_works ... ok
