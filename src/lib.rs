548ac1ebdee423dbddb8e0cf34494fccf034e939
Sam Liu <ontouchstart@gmail.com>
2026-02-02 11:33:04 -0500

    echo_hello_world_v2

--- src/lib.rs
+++ src/lib.rs
@@ -4,21 +4,33 @@
 //! ```
 //! use std::process::Command;
 //! let output = Command::new("echo")
-//!        .arg("Hello,")
-//!        .arg("world!")
-//!        .output()
-//!        .expect("Failed to execute command");
+//!     .arg("Hello,")
+//!     .arg("world!")
+//!     .output()
+//!     .expect("Failed to execute command");
 //! let output_1 = String::from_utf8_lossy(&output.stdout).to_string();
 //! assert_eq!(format!("{}", output_1), "Hello, world!\n");
 //! let output_2 = String::from_utf8(output.stdout).expect("Format error");
 //! assert_eq!(format!("{}", output_2), "Hello, world!\n");
-//!
 //! ```
 //! [std::process::Command]
 //!
 //! [String::from_utf8]
 //!
 //! [String::from_utf8_lossy]
+//!
+//! After RTFM, we might want to use [str::from_utf8]:
+//!
+//! ```
+//! use std::process::Command;
+//! let output = Command::new("echo")
+//!        .arg("Hello,")
+//!        .arg("world!")
+//!        .output()
+//!        .expect("Failed to execute command");
+//! let hello_world = str::from_utf8(&output.stdout).expect("Format error");
+//! assert_eq!(hello_world, "Hello, world!\n");
+//! ```
 
 use std::process::Command;
 
@@ -31,8 +43,24 @@ pub fn echo_hello_world() -> String {
     String::from_utf8(output.stdout).expect("Format error")
 }
 
+pub fn echo_hello_world_v2() -> String {
+    let output = Command::new("echo")
+        .arg("Hello,")
+        .arg("world!")
+        .output()
+        .expect("Failed to execute command");
+    str::from_utf8(&output.stdout)
+        .expect("Format error")
+        .to_string()
+}
+
 #[cfg(test)]
 #[test]
 fn test_echo_hello_world() {
     assert_eq!(format!("{}", echo_hello_world()), "Hello, world!\n");
+}
+
+#[test]
+fn test_echo_hello_worl_v2() {
+    assert_eq!(echo_hello_world_v2(), "Hello, world!\n");
 }
